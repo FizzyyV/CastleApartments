b99594f12c12f1bc838595bb4c4560f7d2921dc3
Erza2727 <eyrun.inga.einarsdottir@gmail.com>
2025-05-07 13:37:18 +0000

    added user to settings.py and added information to the modles.py folder in the user and migrated to create a sql

--- user/models.py
+++ user/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
+
+class User(models.Model):
+    firstname = models.CharField(max_length=255)
+    lastname = models.CharField(max_length=255)
+
+
+from django.db import models
+
 # Create your models here.
