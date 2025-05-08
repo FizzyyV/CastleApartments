bb8cec8271fe23c67cb2d91a7b2da38f8bf75610
soleynott <soleynott@gmail.com>
2025-05-08 16:28:19 +0000

    fixing untracked

---
+++ property/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+
+from . import views
+urlpatterns = [
+
+    # http://localhost:9000 (root)
+    path('', views.index, name='property-index'),
+
+    # http://localhost:9000/123 (id NUM)
+    path('<int:property_id>/', views.get_property_by_id, name='property-by-id'),
+
+    #path('<int:property_id>/submit_offer', views.submit_offer, name='property-submit-offer'),
+]
