4cdd03a3a64a8488772a45706fbdc7c556b848e3
Erza2727 <eyrun.inga.einarsdottir@gmail.com>
2025-05-08 10:44:28 +0000

    conected property to user by giving seller an address using the address class in property

--- user/models.py
+++ user/models.py
@@ -1,6 +1,7 @@
 
 # Create your models here.
 from django.contrib.auth.models import AbstractUser
+from property.models import Property, Address
 from django.db import models
 
 # ====================
@@ -15,7 +16,7 @@ class User(AbstractUser):
     profile_image = models.ImageField(upload_to='profiles/', blank=True, null=True)
 
 # ====================
-# Buyer Profile (optional)
+# Buyer Profile
 # ====================
 class Buyer(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
@@ -26,15 +27,9 @@ class Buyer(models.Model):
 class Seller(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     seller_type = models.CharField(max_length=50)  # 'individual' or 'agency'
-    address = models.CharField(max_length=255, blank=True)
+    seller_Address = models.ForeignKey(Address, on_delete=models.SET_NULL, null=True, blank=True)
     logo = models.ImageField(upload_to='logos/', blank=True, null=True)
     cover_image = models.ImageField(upload_to='covers/', blank=True, null=True)
     bio = models.TextField(blank=True)
-
-
-
-# ====================
-# Purchase Offer
-# ====================
 
 
