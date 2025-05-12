6c07503fc1b855c543ebee375af81593bdc0f6d0
Erza2727 <eyrun.inga.einarsdottir@gmail.com>
2025-05-12 11:07:50 +0000

    bs

---
+++ account/models.py
@@ -0,0 +1,35 @@
+
+# Create your models here.
+from django.contrib.auth.models import AbstractUser
+from property.models import Property, Address
+from django.db import models
+
+# ====================
+# Custom User Model
+# ====================
+class User(AbstractUser):
+    ROLE_CHOICES = (
+        ('buyer', 'Buyer'),
+        ('seller', 'Seller'),
+    )
+    role = models.CharField(max_length=10, choices=ROLE_CHOICES)
+    profile_image = models.ImageField(upload_to='profiles/', blank=True, null=True)
+
+# ====================
+# Buyer Profile
+# ====================
+class Buyer(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+# ====================
+# Seller Profile
+# ====================
+class Seller(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    seller_type = models.CharField(max_length=50)  # 'individual' or 'agency'
+    seller_Address = models.ForeignKey(Address, on_delete=models.SET_NULL, null=True, blank=True)
+    logo = models.ImageField(upload_to='logos/', blank=True, null=True)
+    cover_image = models.ImageField(upload_to='covers/', blank=True, null=True)
+    bio = models.TextField(blank=True)
+
+
