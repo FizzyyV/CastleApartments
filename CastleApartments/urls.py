ffaf680a7d898246aafae1f4287295b4b8dea21b
FizzyyV <marta.a.hilmarsdottir@gmail.com>
2025-05-14 11:41:23 +0000

    fix login

--- CastleApartments/urls.py
+++ CastleApartments/urls.py
@@ -30,10 +30,7 @@ urlpatterns = [
     # http://localhost:5000/admin
     path('admin/', admin.site.urls),
 
-    #MAKE THIS WORK
-    #path('login/', SOMETHING)
-
-    path('login/', auth_views.LoginView.as_view(template_name='account/login_pretty.html'), name='login'),
+    path('accounts/login/', auth_views.LoginView.as_view(template_name='account/login_pretty.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
 
     #maybe dont need
