c0e6247a2f4b782bf4c8ee5df2bfe7339c56c1bb
Erza2727 <eyrun.inga.einarsdottir@gmail.com>
2025-05-16 12:23:28 +0000

    fixed account/urls

--- account/urls.py
+++ account/urls.py
@@ -12,7 +12,11 @@ urlpatterns = [
     path('logout/', LogoutView.as_view(), name='logout'),
     path('profile/', views.profile, name='profile'),
 
-    path('login/', views.custom_login, name='login'),
+    path('login/', LoginView.as_view(template_name='account/login.html', redirect_authenticated_user=True),
+         name='login'),
 
+    #path('login/', views.custom_login, name='login'),
+    #path("signup/", views.custom_signup(), name="signup"),
+    #path("profile/", views.profile_view, name='profile'),
 ]
 
